8794ae945cef3d13c7a2bfc4242c1004b839f6fa
Enrico Bianchi <enrico.bianchi@gmail.com>
2013-06-28 00:43:00 +0200

    Review command line option's system

--- src/main/java/it/application/yds/Main.java
+++ src/main/java/it/application/yds/Main.java
@@ -4,7 +4,6 @@
  * Description   The Yggdrasill Document Search - A java based file indexer
  * License       BSD (see LICENSE.BSD for details)
  */
-
 package it.application.yds;
 
 import it.application.yds.clean.Cleaner;
@@ -15,6 +14,13 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Properties;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
 import org.apache.log4j.Appender;
 import org.apache.log4j.FileAppender;
 import org.apache.log4j.Level;
@@ -26,20 +32,50 @@ import org.apache.log4j.PatternLayout;
  * @author enrico
  */
 public class Main {
+
     public static final String PROGNAME = "YDS";
     public static final String VERSION = "0.3";
     public static final Logger logger = Logger.getLogger("YDS");
-    private ArrayList<String> queryData;
-    private Boolean clean;
-    private Boolean query;
-    private Boolean scan;
     private Properties cfgFile;
+    private Options opts;
 
     public Main() {
-        this.queryData = new ArrayList<>();
-        this.clean = Boolean.FALSE;
-        this.query = Boolean.FALSE;
-        this.scan = Boolean.FALSE;
+        this.opts = new Options();
+
+        this.opts.addOption("h", "help", false, "Print this help");
+        this.opts.addOption(OptionBuilder
+                .withLongOpt("cfg")
+                .withDescription("Set the configuration file")
+                .hasArg()
+                .withArgName("FILE")
+                .create("c"));
+        this.opts.addOption(OptionBuilder
+                .withLongOpt("scan")
+                .withDescription("Scan data")
+                .create("s"));
+        this.opts.addOption(OptionBuilder
+                .withLongOpt("clean")
+                .withDescription("Remove old indexed data")
+                .create("C"));
+        this.opts.addOption(OptionBuilder
+                .withLongOpt("query")
+                .hasArg()
+                .withArgName("ARG")
+                .withDescription("Query data")
+                .create("q"));
+        this.opts.addOption(OptionBuilder
+                .withLongOpt("cmd")
+                .withDescription("Use command line for querying data")
+                .create());
+    }
+
+    /**
+     * Print various information about the application
+     */
+    private void printHelp(Integer code) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("YDS", this.opts);
+        System.exit(code);
     }
 
     /**
@@ -47,7 +83,7 @@ public class Main {
      */
     private void setLog() {
         Appender appender;
-        
+
         try {
             appender = new FileAppender(new PatternLayout("%d %-5p %c - %m%n"), this.cfgFile.getProperty("log_file"));
             Main.logger.addAppender(appender);
@@ -60,6 +96,7 @@ public class Main {
 
     /**
      * Open the configuration file
+     *
      * @param cfgFile a configuration file
      */
     private void setCfg(String cfgFile) {
@@ -72,110 +109,91 @@ public class Main {
         }
     }
 
-    /**
-     * Print various information about the application
-     */
-    public void printHelp() {
-        System.out.println(Main.PROGNAME + " " + Main.VERSION);
-        System.out.println("Usage:");
-        System.out.println("    -c<file> or --cfg=<file> Set the configuration file");
-        System.out.println("    -s or --scan             Scan data");
-        System.out.println("    -w or --wipe             Remove old indexed data");
-        System.out.println("    -q or --query            Query data");
-        System.out.println("    -h or --help             Print this help");
-    }
-
-    /**
-     * Parser for parameters passed via command line
-     * @param option a parameter
-     */
-    public void parseOption(String option) throws IllegalArgumentException {
-        if (option.startsWith("-c") ||
-            option.startsWith("--cfg=")) {
-            if (option.startsWith("--")) {
-                this.setCfg(option.substring(6));
-            } else {
-                this.setCfg(option.substring(2));
-            }
-        } else if (option.equals("-s") ||
-                   option.equals("--scan")) {
-            this.scan = Boolean.TRUE;
-        } else if (option.equals("-q") ||
-                   option.equals("--query")) {
-            this.query = Boolean.TRUE;
-        } else if (option.equals("-h") ||
-                   option.equals("--help")) {
-            this.printHelp();
-            System.exit(0);
-        } else if (option.equals("-w") ||
-                   option.equals("--wipe")) {
-            this.clean = Boolean.TRUE;
-        } else if (this.query.booleanValue()) {
-            // All parameters after -q or --query are used for query data
-            this.queryData.add(option);
-        } else {
-            System.out.println("Invalid parameter");
-            this.printHelp();
-            System.exit(3);
-        }
-    }
-
-    public void execute() {
-        Fetcher fetch;
+    public void go(String[] args) throws ParseException {
         Cleaner aCleaner;
-        Query queries;
+        CommandLine cmd;
+        CommandLineParser parser;
+        Fetcher fetch;
+        Query query;
+
+        parser = new PosixParser();
+        cmd = parser.parse(this.opts, args);
+
+        if (cmd.hasOption("help")) {
+            this.printHelp(0);
+        }
+
+        if (cmd.hasOption("cfg")) {
+            this.setCfg(cmd.getOptionValue("cfg"));
+        } else {
+            System.out.println("No configuration file defined (see help)");
+            System.exit(2);
+        }
 
         this.setLog();
 
-        if (this.clean.booleanValue()) {
+        if (cmd.hasOption("clean")) {
             try {
                 aCleaner = new Cleaner(cfgFile);
                 aCleaner.start();
+                System.exit(0);
             } catch (IllegalArgumentException | SQLException ex) {
                 Main.logger.error(null, ex);
+                System.exit(3);
             }
-        } else if (this.scan.booleanValue()) {
+        }
+
+        if (cmd.hasOption("scan")) {
             try {
                 fetch = new Fetcher(this.cfgFile);
                 fetch.start();
+                System.exit(0);
             } catch (IllegalArgumentException | SQLException ex) {
                 Main.logger.error(null, ex);
+                System.exit(3);
             }
-        } else if (this.query.booleanValue()) {
-            try {
-                queries = new Query(cfgFile);
-                if (this.queryData.size() > 0) {
-                    for (String value: this.queryData) {
-                        queries.search(value);
-                    }
-                } else {
-                    queries.search();
-                }
-            } catch (IllegalArgumentException | SQLException ex) {
-                Main.logger.error(null, ex);
-            }
-        } else {
-            System.out.println("None operation is selected");
-            this.printHelp();
-            System.exit(1);
         }
+
+        if (cmd.hasOption("query")) {
+            try {
+                query = new Query(cfgFile);
+                query.search(cmd.getOptionValue("query"));
+
+                System.exit(0);
+            } catch (IllegalArgumentException | SQLException ex) {
+                Main.logger.error(null, ex);
+                System.exit(3);
+            }
+        }
+
+        if (cmd.hasOption("cmd")) {
+            try {
+                query = new Query(cfgFile);
+                query.search();
+            } catch (IllegalArgumentException | SQLException ex) {
+                Main.logger.error(null, ex);
+                System.exit(3);
+            }
+        }
+
+        if (cmd.getOptions().length <= 0) {
+            System.out.println("None operation is selected");
+            this.printHelp(1);
+        }
+
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Main m;
+        try {
+            Main m;
 
-        m = new Main();
-        if (args.length > 0) {
-            for (int i = 0; i < args.length; i++) {
-                m.parseOption(args[i]);
-            }
-            m.execute();
-        } else {
-            m.printHelp();
-            System.exit(0);
+            m = new Main();
+            m.go(args);
+        } catch (ParseException ex) {
+            Main.logger.error(null, ex);
         }
     }
 }
